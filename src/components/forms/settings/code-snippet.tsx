2cb3fadd038374e5b778784724716818c98baa55
danil2w3 <148650024+danil2w3@users.noreply.github.com>
2024-12-07 04:56:56 +0600

    Update code-snippet.tsx

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -8,6 +8,8 @@ type Props = {
   id: string
 }
 
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000'
+
 const CodeSnippet = ({ id }: Props) => {
   const { toast } = useToast()
   let snippet = `
@@ -28,16 +30,16 @@ const CodeSnippet = ({ id }: Props) => {
         }
     ')
     
-    iframe.src = "http://localhost:3000/chatbot"
+    iframe.src = "${BASE_URL}/chatbot"
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
     
     window.addEventListener("message", (e) => {
-        if(e.origin !== "http://localhost:3000") return null
+        if(e.origin !== "${BASE_URL}") return null
         let dimensions = JSON.parse(e.data)
         iframe.width = dimensions.width
         iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
+        iframe.contentWindow.postMessage("${id}", "${BASE_URL}/")
     })
         `
 
