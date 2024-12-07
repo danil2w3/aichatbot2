19c0c8e671bd2b47b0f3984600c73356eb623ca5
danil2w3 <148650024+danil2w3@users.noreply.github.com>
2024-12-07 06:46:00 +0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,20 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/auth(.*)', '/portal(.*)', '/images(.*)'],
+  publicRoutes: ['/', '/auth(.*)', '/portal(.*)', '/images(.*)', '/favicon.ico'],
   ignoredRoutes: ['/chatbot'],
+
+  afterAuth(auth, req) {
+    const { pathname } = req.nextUrl;
+    const protectedPages = ['/settings', '/conversation', '/dashboard', '/integration', '/integration', '/appointment'];
+  
+    if (!auth.userId && protectedPages.includes(pathname)) {
+      const signInUrl = new URL('/auth/sign-in', req.url);
+      return Response.redirect(signInUrl);
+    }
+  }
+  
+});
 })
 
 export const config = {
